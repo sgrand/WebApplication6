51026c29865d3045bc3fa0c3b563a689f0733f9d
Grand Sébastien <sgrand@live.ca>
2016-02-22 18:33:21 -0500

    Initial AspNet* tables deployed to database (http://docs.asp.net/projects/mvc/en/latest/getting-started/first-mvc-app/adding-model.html)

--- src/WebApplication6/Migrations/20160222233156_Initial.Designer.cs
+++ src/WebApplication6/Migrations/20160222233156_Initial.Designer.cs
@@ -8,9 +8,10 @@ using WebApplication6.Models;
 namespace WebApplication6.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160222233156_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
